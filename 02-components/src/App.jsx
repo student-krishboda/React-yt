82012b4053309abdb4da3d2b572fdb7ea487a811
student-krishboda <krishboda18@gmail.com>
2026-06-16 09:54:29 +0530

    01-react-folder

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className='card'>
+        <h1> krish </h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum, veniam perspiciatis voluptates minus maiores iusto nihil doloremque odio, reiciendis, consequatur minima quidem? Porro illum sequi reprehenderit dicta dolorem placeat commodi?</p>     
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ my-react-app/src/main.jsx
@@ -0,0 +1,2 @@
+import React from 'react'
+import Reaction from 'react-dom/client'
